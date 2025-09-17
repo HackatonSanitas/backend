7419a19098d3e6719f7b4136ad75e2e2d17d84d9
Sofía Santos García <sofiasantos@MacBook-Air-de-Sofia.local>
2025-09-17 22:20:28 +0200

    feat(MedicationSchedule): add medication schedule attributes

--- src/main/java/com/example/sanitas/medication_schedules/MedicationSchedule.java
+++ src/main/java/com/example/sanitas/medication_schedules/MedicationSchedule.java
@@ -1,11 +1,15 @@
 package com.example.sanitas.medication_schedules;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.Table;
+import com.example.sanitas.medications.Medication;
+import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.LocalTime;
 
 @Entity
 @Table(name = "medications_schedules")
@@ -14,4 +18,26 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 @Builder
 public class MedicationSchedule {
+    @Id
+    @GeneratedValue (strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "medication_id", nullable = false)
+    private Medication medication;
+
+    @Column(nullable = false)
+    private LocalDate scheduleDate;
+
+    @Column(nullable=false)
+    private LocalTime scheduleTime;
+
+    @Column(name = "created_at")
+    private LocalDateTime createdAt;
+
+    @PrePersist
+    protected void onCreate(){
+        createdAt = LocalDateTime.now();
+    }
+
 }
