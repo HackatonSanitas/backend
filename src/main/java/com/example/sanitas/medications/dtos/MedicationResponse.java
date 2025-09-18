110267f34483dada7467edc19d1902efe0435d95
Sofía Santos García <sofiasantos@MacBook-Air-de-Sofia.local>
2025-09-18 10:55:45 +0200

    feat(MedicationResponse): add medication response fields medication, dose and frequency, intake and status

--- src/main/java/com/example/sanitas/medications/dtos/MedicationResponse.java
+++ src/main/java/com/example/sanitas/medications/dtos/MedicationResponse.java
@@ -1,4 +1,10 @@
 package com.example.sanitas.medications.dtos;
 
-public record MedicationResponse() {
+public record MedicationResponse(
+        String medication,
+        String dose,
+        String frequency,
+        String intake,
+        String status
+) {
 }
