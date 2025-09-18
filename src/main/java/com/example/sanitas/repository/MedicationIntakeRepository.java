b98972bf8d8e8715fc4c0d852a5263429130e990
Sofía Santos García <sofiasantos@MacBook-Air-de-Sofia.local>
2025-09-18 12:31:11 +0200

    fix(MedicationIntakeRepository): fix name

--- src/main/java/com/example/sanitas/repository/MedicationIntakeRepository.java
+++ src/main/java/com/example/sanitas/repository/MedicationIntakeRepository.java
@@ -1,6 +1,7 @@
 package com.example.sanitas.repository;
 
+import com.example.sanitas.models.MedicationIntake;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface MedicationIntakeRepository extends JpaRepository {
+public interface MedicationIntakeRepository extends JpaRepository<MedicationIntake, Long> {
 }
