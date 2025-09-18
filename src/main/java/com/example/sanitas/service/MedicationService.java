c1e37b1db5541b07f88c2ed718b0f131490692e9
Sofía Santos García <sofiasantos@MacBook-Air-de-Sofia.local>
2025-09-18 12:30:24 +0200

    feat(MedicationService): add getActiveMedication

--- src/main/java/com/example/sanitas/service/MedicationService.java
+++ src/main/java/com/example/sanitas/service/MedicationService.java
@@ -1,36 +1,21 @@
 package com.example.sanitas.service;
 
-// TODO: Add "import" x2 for "Medication Entity" and "Medication Service"
 
+import com.example.sanitas.models.Medication;
+import com.example.sanitas.repository.MedicationIntakeRepository;
+import com.example.sanitas.repository.MedicationRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
-import java.time.LocalDateTime;
 import java.util.List;
 
 @Service
-@RequiredArgsConstructor // Lombok - It should creates constructor with required (final) fields
+@RequiredArgsConstructor
 public class MedicationService {
     private final MedicationRepository medicationRepository;
-
-    public Medication createMedication(Medication medication) {
-        return medicationRepository.save(medication);
-    }
+    private final MedicationIntakeRepository medicationIntakeRepository;
 
     public List<Medication> getActiveMedications() {
-        return medicationRepository.findByIsActiveTrue();
-    }
-
-    public void deleteMedication(Long id) {
-        // Soft delete
-        Medication medication = medicationRepository.findById(id).orElseThrow();
-        medication.setIsActive(false);
-        medicationRepository.save(medication);
-    }
-
-    public Medication markAsTaken(Long id) {
-        Medication medication = medicationRepository.findById(id).orElseThrow();
-        medication.setLastTaken(LocalDateTime.now());
-        return medicationRepository.save(medication);
+        return medicationRepository.findAll();
     }
 }
