2a9d9de1eb5940600cc6ab78b167f74a14946e24
Sofía Santos García <sofiasantos@MacBook-Air-de-Sofia.local>
2025-09-18 14:30:53 +0200

    fix(MedicationMapper): fix conexion with frontend

--- src/main/java/com/example/sanitas/dtos/MedicationMapper.java
+++ src/main/java/com/example/sanitas/dtos/MedicationMapper.java
@@ -1,14 +1,19 @@
 package com.example.sanitas.dtos;
 
 import com.example.sanitas.models.Medication;
+import com.example.sanitas.models.Status;
 
 public class MedicationMapper {
 
     public static Medication toEntity(MedicationRequest dto){
+        String frequency = dto.days() == 1 ? "Una vez" : "Cada " + dto.days() + " días";
         return Medication.builder()
                 .medication(dto.medication())
                 .dose(dto.dose())
-                .frequency(dto.frequency())
+                .frequency(frequency)
+                .nextDate(dto.startDate())
+                .nextTime(dto.schedule())
+                .status(Status.PENDING)
                 .build();
     }
 
@@ -17,7 +22,7 @@ public class MedicationMapper {
                 medication.getMedication(),
                 medication.getDose(),
                 medication.getFrequency(),
-                medication.getIntakes().toString(),
+                medication.getNextDate().toString() + " " + (medication.getNextTime() != null ? medication.getNextTime().toString() : ""),
                 medication.getStatus().name()
         );
     }
