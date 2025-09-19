a40b6bb84b4ce0ac5d1e18fe2f9b97efd08e02d5
Sofía Santos García <sofiasantos@MacBook-Air-de-Sofia.local>
2025-09-19 09:03:57 +0200

    fix(CorsConfig): delete comments

--- src/main/java/com/example/sanitas/config/CorsConfig.java
+++ src/main/java/com/example/sanitas/config/CorsConfig.java
@@ -1,4 +1,4 @@
-package com.example.sanitas.config; // (!) Package path 
+package com.example.sanitas.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -13,15 +13,10 @@ public class CorsConfig {
         return new WebMvcConfigurer() {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
-                // It should allow "ALL endpoint patterns" (/api, /auth, etc.)
                 registry.addMapping("/**")
-                        // This is the URL of the React App (Frontend Team):
-                        .allowedOrigins("http://localhost:5173") // (!) It should match the Vite server's URL!
-                        // Allow these HTTP methods
+                        .allowedOrigins("http://localhost:5173")
                         .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
-                        // Allow all headers
                         .allowedHeaders("*")
-                        // Allow cookies/authentication "in case of" use them later:
                         .allowCredentials(true);
             }
         };
